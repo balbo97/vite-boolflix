d55554191ef08cfb18d57b1e9d6421b698c7167b
balbo97 <balbosauro1@gmail.com>
2024-01-12 16:33:15 +0100

    MILESTONE 0 completed [store.js created ]

---
+++ src/store.js
@@ -0,0 +1,7 @@
+import { reactive } from 'vue';
+
+export const store = reactive({
+    endpointMovies: 'https://api.themoviedb.org/3/search/movie?api_key',
+    apiKey: 'a4621505ed8c00f472a6b2c7247e5d63'
+
+})
