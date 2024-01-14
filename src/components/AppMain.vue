1428031619db4aae496784cf1089df47e92c9cac
balbo97 <balbosauro1@gmail.com>
2024-01-14 22:29:19 +0100

    milestone-1 completed

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,15 +1,35 @@
 <script>
+import { store } from '../store'
 export default {
+    data() {
+        return {
+            store
+        }
+    },
 
 }
 </script>
 
 <template lang="">
-    <div>
-        
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col-12 col-md-6 col-lg-3 my-2"  v-for="movie in store.movieList">
+                
+                <div class="cards">
+                    <h2>{{movie.title}}</h2>
+                    <h6>{{movie.original_title}}</h6>
+                    <p>{{movie.original_language}}</p>
+                    <p>{{movie.vote_average}}</p>
+                    
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
-<style lang="">
-    
+<style lang="scss" scoped>
+.cards {
+    background-color: white;
+    padding: 30px;
+}
 </style>
